c3e38f281826e9bcc9f077acea3ebce4f375445e
Igor Botini <igorbotinii@gmail.com>
2025-04-25 20:24:04 -0300

    Primeiro

---
+++ src/main/java/com/mycompany/spotifei/Spotifei.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spotifei;
+
+/**
+ *
+ * @author igorb
+ */
+public class Spotifei {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
