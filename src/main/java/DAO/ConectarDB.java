f7f3d62acf453944c0cf92c58aec615d995bd63c
Igor Botini <igorbotinii@gmail.com>
2025-04-29 20:58:21 -0300

    Cadastrando usuario

--- src/main/java/DAO/ConectarDB.java
+++ src/main/java/DAO/ConectarDB.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConectarDB {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/db_spotifei"; // nome do seu banco aqui
+    private static final String URL = "jdbc:postgresql://localhost:5432/db_spotifei"; 
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "1qaz2wsx3EDC@@!"; // coloque a senha que você definiu
+    private static final String SENHA = "1qaz2wsx3EDC@@!";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
