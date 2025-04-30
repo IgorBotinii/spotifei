6c9c79059618bb8330fca2a5ce2f168378b76681
Igor Botini <igorbotinii@gmail.com>
2025-04-29 21:53:19 -0300

    Cadastrando usuario

--- src/main/java/DAO/CadNovoUser.java
+++ src/main/java/DAO/CadNovoUser.java
@@ -10,14 +10,14 @@ public class CadNovoUser {
     public void inserirUsuario(Usuario user) {
         String sql = "INSERT INTO cd_usuarios (nome_user, senha_user, nome_comp_user) VALUES (?, ?, ?)";
 
-        try (Connection conn = ConectarDB   .conectar();
+        try (Connection conn = ConectarDB.conectar();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, user.getNomeUsuario());
             stmt.setString(2, user.getSenhaUsuario());
             stmt.setString(3, user.getNomeCompleto());
 
-            stmt.executeUpdate();
+            stmt.executeUpdate();   
             System.out.println("Usuário inserido com sucesso!");
 
         } catch (SQLException e) {
